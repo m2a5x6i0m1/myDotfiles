f326792f823c264a3ed5ea4fed55038adb5b2897
m2a5x6i0m1 <max25601new@gmail.com>
2025-08-01 15:36:54 +0300

    better config for lua_ls

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
 			capabilities = require("blink.cmp").get_lsp_capabilities(),
 			root_markers = { ".git" },
 		})
+
 		vim.lsp.enable({ "nixd", "bashls", "harper_ls", "lua_ls", "clangd", "rust_analyzer" })
 	end,
 }
