7ee08f406185c3966b4fa512f1ac9dc660d42a60
m2a5x6i0m1 <personal.maxim.yanchenko@gmail.com>
2025-11-13 14:27:13 +0300

    +bullets.nvim

--- nvim/.config/nvim/lua/plugins/todo.lua
+++ nvim/.config/nvim/lua/plugins/todo.lua
@@ -2,4 +2,7 @@ return {
 	"folke/todo-comments.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("todo-comments").setup()
+	end,
 }
